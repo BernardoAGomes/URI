69ec9cfde09ee464f96c2d9e16dbaef5fbcf311b
Bernardo Amaral Gomes <bernardoamaralgomes@hotmail.com>
2026-03-04 19:08:46 -0300

    Add files via upload

---
+++ Media/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Produto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double MEDIA;
+
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double pesoA = 2;
+            double pesoB = 3;
+            double pesoC = 5;
+            MEDIA = (a * pesoA + b * pesoB + c * pesoC) /(pesoA + pesoB + pesoC);
+
+
+
+            Console.WriteLine("MEDIA = " + MEDIA.ToString("F1"));
+            Console.ReadLine();
+
+        }
+    }
+}
