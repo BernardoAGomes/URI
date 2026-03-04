d82f5226f3624b18f0fc162dc1039e4d82500e73
Bernardo Amaral Gomes <bernardoamaralgomes@hotmail.com>
2026-03-04 19:25:23 -0300

    Add files via upload

---
+++ SalarioComBonus/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vendedor = Console.ReadLine(); 
+            double fixo = double.Parse(Console.ReadLine());
+            double vendas = double.Parse(Console.ReadLine());
+            double salario;
+            salario = fixo + (vendas * 0.15);
+
+           
+
+           
+            Console.WriteLine("TOTAL = R$ " + salario.ToString("F2"));
+            Console.ReadLine();
+
+        }
+    }
+}
