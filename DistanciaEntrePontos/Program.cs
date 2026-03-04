3f2549ad9a8c66b30551ebf9e89b3415b745f9e7
Bernardo Amaral Gomes <bernardoamaralgomes@hotmail.com>
2026-03-04 19:20:17 -0300

    Add files via upload

---
+++ DistanciaEntrePontos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace DistanciaEntreDoisPontos;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x1, y1, x2, y2, distancia;
+        string[] vet = Console.ReadLine().Split(' ');
+        x1 = double.Parse(vet[0]);
+        y1 = double.Parse(vet[1]);
+        string[] vet2 = Console.ReadLine().Split(' ');
+        x2 = double.Parse(vet2[0]);
+        y2 = double.Parse(vet2[1]);
+
+        distancia = Math.Sqrt(Math.Pow((x2 - x1), 2.0) + (Math.Pow((y2 - y1), 2.0)));
+
+        Console.WriteLine(distancia.ToString("F4"));
+
+
+        Console.ReadLine();
+
+    }
+}
