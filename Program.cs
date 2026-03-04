e04b15ed81ed38ede490f62bf68cf1fcd6dedc71
Bernardo Amaral Gomes <bernardoamaralgomes@hotmail.com>
2026-03-04 18:00:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace AreaDoCirculo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        double area, n, raio, y;
+
+        
+        raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+     
+        n = 3.14159;
+        
+
+        area = n * raio * raio;
+
+        Console.WriteLine("A = " + area.ToString("F4"), CultureInfo.InvariantCulture);
+            
+
+        Console.ReadLine();
+
+
+
+    }
+}
+
