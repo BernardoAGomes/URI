ea20d5a4a8b16a58c948c8f501d5110f842b6f99
Bernardo Amaral Gomes <bernardoamaralgomes@hotmail.com>
2026-03-04 19:13:20 -0300

    Add files via upload

---
+++ Salario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int funcionario = int.Parse(Console.ReadLine());
+            int horas = int.Parse(Console.ReadLine());
+            double valor = double.Parse(Console.ReadLine());
+            double salario;
+
+            salario = horas * valor;
+
+
+            Console.WriteLine("NUMBER = " + funcionario);
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2"));
+            Console.ReadLine();
+
+        }
+    }
+}
