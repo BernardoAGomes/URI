692b4f3fb0dc73587fc42a666b444593b2bd4bfd
Bernardo Amaral Gomes <bernardoamaralgomes@hotmail.com>
2026-03-10 12:30:43 -0300

    Add files via upload

---
+++ Triangulo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+
+
+namespace Triangulo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double A, B, C, area, perimetro;
+        string[] vet = Console.ReadLine().Split(' ');
+        A = double.Parse(vet[0]);
+        B = double.Parse(vet[1]);
+        C = double.Parse(vet[2]);
+
+
+        if (A < B + C && B < A + C && C < A + B)
+        {
+            perimetro = A + B + C;
+            Console.WriteLine("Perimetro = " + perimetro.ToString("F1"));
+        } else
+        {
+            area = ((A + B) * C) / 2;
+            Console.WriteLine("Area = " + area.ToString("F1"));
+        }
+
+        Console.ReadLine();
+
+    }
+}
