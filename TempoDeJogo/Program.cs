7316231bd88530fcd30c321cc01b7cafa440bb18
Bernardo Amaral Gomes <bernardoamaralgomes@hotmail.com>
2026-03-10 12:35:17 -0300

    Add files via upload

---
+++ TempoDeJogo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+
+
+namespace TempoDeJogo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int Hi, Hf, jogo;
+        string[] vet = Console.ReadLine().Split(' ');
+        Hi = int.Parse(vet[0]); 
+        Hf = int.Parse(vet[1]);
+
+        if (Hi < Hf)
+        {
+            jogo = Hf - Hi;
+        }
+        else
+        {
+            jogo = 24 - Hi + Hf;
+        }
+        Console.WriteLine("O JOGO DUROU " + jogo + " HORA(S)");
+        
+        
+            Console.ReadLine();
+
+
+
+    }
+}
