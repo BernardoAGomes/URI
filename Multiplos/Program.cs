445c2e6d8c198948c5d2c69b9556d6e9044eb40d
Bernardo Amaral Gomes <bernardoamaralgomes@hotmail.com>
2026-03-10 12:32:29 -0300

    Add files via upload

---
+++ Multiplos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+
+
+namespace EstruturaCondicional;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int A, B;
+        string[] vet = Console.ReadLine().Split(' ');
+        A = int.Parse(vet[0]);
+        B = int.Parse(vet[1]);
+
+
+        if (B % A == 0 || A % B == 0)
+        {
+            Console.WriteLine("Sao Multiplos");
+        }
+        else
+        {
+            Console.WriteLine("Nao sao Multiplos");
+        }
+            Console.ReadLine();
+
+
+
+    }
+}
